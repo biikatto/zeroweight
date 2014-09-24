26c3cd1250e088f781df278961d4894b04121b0c
Ness Morris <karplusstrong@gmail.com>
2014-09-23 19:09:52 -0700

    Added Target.cs

---
+++ Assets/Target.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour{
+
+	public int maxHitPoints = 100;
+	public float explosionForce = 300f;
+	public bool debug = true;
+
+	int hp;
+	Vector3[] childPositions;
+	Quaternion[] childRotations;
+
+	bool destroyed = false;
+
+	void Start(){
+		hp = maxHitPoints;
+		childPositions = new Vector3[transform.childCount];
+		childRotations = new Quaternion[transform.childCount];
+		for(int i=0;i<transform.childCount;i++){
+			Transform child = transform.GetChild(i);
+			child.rigidbody.isKinematic = true;
+			childPositions[i] = child.transform.position;
+			childRotations[i] = child.transform.rotation;
+		}
+	}
+
+	void Update(){
+		if(debug){
+			if(Input.GetButtonDown("Debug")){
+				Reset();
+			}
+		}
+	}
+
+	public void Explode(Vector3 explosionPos){
+		if(!destroyed){
+			foreach(Transform child in transform){
+				child.rigidbody.isKinematic = false;
+				child.rigidbody.AddExplosionForce(
+						explosionForce,
+						explosionPos,
+						20f);
+			}
+		}
+	}
+
+	public void Explode(){
+		Explode(transform.position);
+	}
+
+	void Reset(){
+		hp = maxHitPoints;
+		for(int i=0;i<transform.childCount;i++){
+			Transform child = transform.GetChild(i);
+			child.rigidbody.isKinematic = true;
+			child.position = childPositions[i];
+			child.rotation = childRotations[i];
+		}
+	}
+
+	void AddDamage(int amount){
+		hp -= amount;
+		if(hp <= 0){
+			Explode();
+		}
+	}
+
+	void Repair(int amount){
+		hp += amount;
+		if(hp > maxHitPoints){
+			hp = maxHitPoints;
+		}
+	}
+}
