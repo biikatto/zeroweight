0bfafb4baf094c11a622e6d94a2e0042b091b18d
Ness Morris <karplusstrong@gmail.com>
2014-10-12 21:19:09 -0700

    Changed Weapon.cs to accomodate new kinetic projectiles

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,35 +4,31 @@ using System.Collections;
 public class Weapon : MonoBehaviour {
 
 	public GameObject weaponBeam;
-	public float cooldownTime = 2f;
-	public float weaponTime = 0.5f;
-	public float weaponDamage = 40f;
-	public float kineticForce = 10f;
-	public float mountWidth = 1f;
-	public float range = Mathf.Infinity;
+	public float cooldownTime = 0.75f;
+	public float weaponDamage = 10f;
+	public float projectileForce = 1000f;
+	public float projectileSize = 1f;
 	
-	private float weaponCountdown;
 	private float cooldown;
 	private bool checkForHit;
 	private bool destroyed = false;
+	private GameObject projectilePrefab;
 
 	public bool debug = true;
 
-	private GameObject activeBeam;
-	private float activeTime;
-	private float beamLength;
-
-	void Start(){}
+	void Start(){
+		projectilePrefab = Resources.Load("Prefabs/Projectile") as GameObject;
+	}
 
 	public void Fire(){
 		if(!destroyed){
 			if(cooldown == 0){
-				activeBeam = Instantiate(weaponBeam, transform.position, transform.rotation) as GameObject;
-				activeBeam.transform.localScale = new Vector3(1f, 1f, range);
-				activeBeam.transform.Translate(Vector3.forward * 0.5f * range);
-				activeBeam.transform.parent = transform;
+				GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation) as GameObject;
+				projectile.rigidbody.AddRelativeForce(Vector3.forward * projectileForce);
+				projectile.transform.localScale = Vector3.one * projectileSize;
+				projectile.transform.Translate(Vector3.forward * projectileSize);
+				projectile.GetComponent<KineticProjectile>().kineticDamage = weaponDamage;
 
-				activeTime += weaponTime;
 				cooldown += cooldownTime;
 
 				checkForHit = true;
@@ -41,49 +37,11 @@ public class Weapon : MonoBehaviour {
 	}
 
 	void ManageTimers(){
-		// if weapon is active
-		if(activeTime > 0){
-			// Hit detection
-			if(checkForHit){
-				int layerMask = 1 << 8; // Select 8th layer (Targets)
-				RaycastHit hit;
-				if(Physics.Raycast(transform.position,
- 					    	transform.TransformDirection(Vector3.forward),
-							out hit,
-							range,
-							layerMask)){
-					Transform target;
-					if(hit.transform.parent != null){
-						target = hit.transform.parent;
-					}else{
-						target = hit.transform;
-					}
-					if(debug){
-						Debug.Log("Hit");
-					}
-					target.BroadcastMessage("AddDamage", weaponDamage);
-					checkForHit = false;
-				}
-			}
-			if(activeTime - Time.deltaTime <= 0){
-				Destroy(activeBeam);
-			}
-			activeTime -= Time.deltaTime;
-		}
-		if(activeTime < 0){
-			activeTime = 0;
-		}
 		if(cooldown > 0){
 			cooldown -= Time.deltaTime;
 		}
 		if(cooldown < 0){
 			cooldown = 0;
-		}
-		if(checkForHit & (activeTime == 0)){
-			if(debug){
-				Debug.Log("Miss");
-			}
-			checkForHit = false;
 		}
 	}
 	
