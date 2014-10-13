9e11d38d4759ff3219c432d434ac6064fb9b3715
Ness Morris <karplusstrong@gmail.com>
2014-10-12 19:08:37 -0700

    Added PlayerDelegate.cs

---
+++ Assets/Scripts/PlayerDelegate.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerDelegate : MonoBehaviour{
+	PlayerControl playerControl;
+	PlayerGUI playerGUI;
+	PlayerHealth playerHealth;
+	Thrust thrust;
+	CameraMove cameraMove;
+	Steer steer;
+
+	Weapon leftWeapon;
+	Weapon rightWeapon;
+
+	void Start(){
+		playerControl = gameObject.GetComponentInChildren<PlayerControl>();	
+		playerGUI = gameObject.GetComponentInChildren<PlayerGUI>();	
+		playerHealth = gameObject.GetComponentInChildren<PlayerHealth>();	
+		thrust = gameObject.GetComponentInChildren<Thrust>();
+		cameraMove = gameObject.GetComponentInChildren<CameraMove>();
+		steer = gameObject.GetComponentInChildren<Steer>();
+
+    	foreach(Weapon weapon in gameObject.GetComponentsInChildren<Weapon>()){
+    		if(weapon.gameObject.name == "Left laser"){
+    			leftWeapon = weapon;
+    		}else if(weapon.gameObject.name == "Right laser"){
+    			rightWeapon = weapon;
+    		}
+    	}
+	}
+
+	public void XThrust(float amount){
+		thrust.XThrust(amount);
+	}
+
+	public void YThrust(float amount){
+		thrust.YThrust(amount);
+	}
+
+	public void ZThrust(float amount){
+		thrust.ZThrust(amount);
+	}
+
+	public void FireLeftWeapon(){
+		leftWeapon.Fire();
+	}
+
+	public void FireRightWeapon(){
+		rightWeapon.Fire();
+	}
+}
