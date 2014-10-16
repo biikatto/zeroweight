1d997ba3ade99b0d2052ebc4a8bc67a70dc26147
Ness Morris <karplusstrong@gmail.com>
2014-10-16 03:03:20 -0700

    Allowed changes to projectile color and cleaned up code

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class Weapon : MonoBehaviour {
 
-	public GameObject weaponBeam;
 	public float cooldownTime = 0.75f;
 	public float weaponDamage = 10f;
 	public float projectileForce = 1000f;
@@ -14,28 +13,43 @@ public class Weapon : MonoBehaviour {
 	private bool checkForHit;
 	private bool destroyed = false;
 	private GameObject projectilePrefab;
+	public string projectileColor;
 
 	public bool debug = true;
 
 	void Start(){
-		projectilePrefab = Resources.Load("Prefabs/Projectile") as GameObject;
+		if(projectileColor == "Blue"){
+			projectilePrefab = Resources.Load(
+			"Prefabs/Bullet_Blue") as GameObject;
+		}else if(projectileColor == "Green"){
+			projectilePrefab = Resources.Load(
+			"Prefabs/Bullet_Green") as GameObject;
+		}else if(projectileColor == "Red"){
+			projectilePrefab = Resources.Load(
+			"Prefabs/Bullet_Red") as GameObject;
+		}else{  // default to yellow
+			projectilePrefab = Resources.Load(
+			"Prefabs/Bullet_Yellow") as GameObject;
+		}
+			
 	}
 
 	public void Fire(){
-		if(!destroyed){
-			if(cooldown == 0){
-				GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation) as GameObject;
-				projectile.rigidbody.mass = projectileMass;
-				projectile.transform.localScale = Vector3.one * projectileSize;
-				projectile.transform.Translate(Vector3.forward * projectileSize);
-				projectile.GetComponent<KineticProjectile>().kineticDamage = weaponDamage;
+		if((!destroyed)&&(cooldown == 0)){
+			GameObject projectile = Instantiate(
+			projectilePrefab, transform.position, transform.rotation) as GameObject;
+			projectile.rigidbody.mass = projectileMass;
+			projectile.transform.localScale = Vector3.one * projectileSize;
+			projectile.transform.Translate(Vector3.forward * projectileSize);
+			projectile.GetComponent<KineticProjectile>().kineticDamage = weaponDamage;
 
-				projectile.rigidbody.AddRelativeForce(transform.parent.parent.rigidbody.velocity + (Vector3.forward * 10 * projectileMass * projectileForce));
+			projectile.rigidbody.AddRelativeForce(
+				transform.parent.parent.rigidbody.velocity + (
+					Vector3.forward * 10 * projectileMass * projectileForce));
 
-				cooldown += cooldownTime;
+			cooldown += cooldownTime;
 
-				checkForHit = true;
-			}
+			checkForHit = true;
 		}
 	}
 
