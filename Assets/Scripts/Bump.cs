c78f66a1b8653ddcf077b0fb0161438eeaffaea6
Ness Morris <ness@kadenze.com>
2014-10-22 14:45:02 -0700

    exposed bump force

--- Assets/Scripts/Bump.cs
+++ Assets/Scripts/Bump.cs
@@ -6,6 +6,7 @@ using System.Collections;
 public class Bump : MonoBehaviour {
 	Thrust thrust;
 	public float disruptTime = 0.15f;
+	public float bumpForce = 1f;
 
 	void Start(){
 		// Find our Thrust component
@@ -13,13 +14,13 @@ public class Bump : MonoBehaviour {
 	}
 
 	void Update(){
+		// Debug
 		if(Input.GetButtonDown("Debug")){
 			Debug.Log(rigidbody.GetPointVelocity(transform.position));
 		}
 	}
 
 	void OnCollisionEnter(Collision collision){
-		float bumpForce = 1f;
 		foreach(ContactPoint contact in collision.contacts){
 			Debug.DrawRay(contact.point, contact.normal, Color.white);
 			rigidbody.AddForce(contact.normal * bumpForce * -1f);
