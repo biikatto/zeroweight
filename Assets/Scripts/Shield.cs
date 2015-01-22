a4f49e140cfeea45bec6b2036edbc78f6eb66a40
Ness Morris <ness@kadenze.com>
2015-01-22 12:44:45 -0800

    Implemented Shield

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -2,9 +2,63 @@ using UnityEngine;
 using System.Collections;
 
 public class Shield : MonoBehaviour {
-    public void BeginShield(){
+
+    private GameObject leftShield;
+    private GameObject rightShield;
+    private PlayerDelegate pDelegate;
+
+    private bool upkeepActive = false;
+
+    [SerializeField]
+    private float energyCost = 1f;
+
+    public float EnergyCost{
+        get{ return energyCost; }
+        set{ energyCost = value; }
+    }
+
+    private void Start(){
+        leftShield = Instantiate(Resources.Load("Prefabs/Player1/HalfShield"), transform.position, Quaternion.Euler(0, 90, 0)) as GameObject;
+        leftShield.transform.parent = transform;
+        rightShield = Instantiate(Resources.Load("Prefabs/Player1/HalfShield"), transform.position, Quaternion.Euler(0, -90, 0)) as GameObject;
+        rightShield.transform.parent = transform;
+
+        leftShield.SetActive(false);
+        rightShield.SetActive(false);
+    }
+
+    public void BeginShieldLeft(){
+        leftShield.SetActive(true);
+        StartCoroutine("EnergyUpkeep");
     }
     
-    public void EndShield(){
+    public void EndShieldLeft(){
+        leftShield.SetActive(false);
+    }
+
+    public void BeginShieldRight(){
+        rightShield.SetActive(true);
+        StartCoroutine("EnergyUpkeep");
+    }
+    
+    public void EndShieldRight(){
+        rightShield.SetActive(false);
+    }
+
+    private IEnumerator EnergyUpkeep(){
+        if(!upkeepActive){
+            upkeepActive = true;
+            while(upkeepActive){
+                if(leftShield.active && rightShield.active){
+                    pDelegate.UseEnergy(Time.deltaTime * energyCost * 2);
+                }else if(leftShield.active || rightShield.active){
+                    pDelegate.UseEnergy(Time.deltaTime * energyCost);
+                }else{
+                    upkeepActive = false;
+                    break;
+                }
+                yield return null;
+            }
+        }
     }
 }
