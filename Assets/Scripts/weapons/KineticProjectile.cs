1815aefd3da201ccd28d9ced8f948b762af4a060
Ness Morris <karplusstrong@gmail.com>
2015-05-05 21:57:12 -0700

    added returning bullet functionality
    which is broken and disabled by default

--- Assets/Scripts/weapons/KineticProjectile.cs
+++ Assets/Scripts/weapons/KineticProjectile.cs
@@ -9,14 +9,39 @@ public class KineticProjectile : MonoBehaviour{
 
 	// Who fired the weapon?
 	private GameObject origin;
+	
+	private new Rigidbody rigidbody;
+	private new Collider collider;
+
+	public bool returns;
+	private bool returning;
 
 	private AudioSource source;
 	public AudioClip impactSound;
 
+	void Start(){
+    	source = gameObject.AddComponent<AudioSource>();
+    	returns = false;
+    	returning = false;
+		rigidbody = gameObject.GetComponent<Rigidbody>();
+		collider = gameObject.GetComponentInChildren<Collider>();
+	}
+
 	void FixedUpdate(){
-		SeekOpponent();
+		if(!returning || !returns){
+			SeekShip(FindOpponent());
+		}else{
+			Debug.Log("pubup");
+			SeekShip(origin);
+		}
 		if((transform.position - origin.transform.position).sqrMagnitude > range*range){
-			Destroy(gameObject);
+			if(returns){
+				returning = true;
+			}else{
+				Destroy(gameObject);
+			}
+			rigidbody.velocity = rigidbody.velocity * -1f;
+			collider.isTrigger = true;
 		}
 	}
 
@@ -25,15 +50,17 @@ public class KineticProjectile : MonoBehaviour{
 	    set{ origin = value; }
 	}
 
-	void Start(){
-    	source = gameObject.AddComponent<AudioSource>();
+	public void OnTriggerEnter(Collider other){
+		if(other.gameObject == origin){
+			Destroy(gameObject);
+		}
 	}
 
 	void OnCollisionEnter(Collision collision){
 		ExplosionEffect();
 
 		ContactPoint contact = collision.contacts[0];
-		if(contact.otherCollider.gameObject.tag == "Player"){
+		if(contact.otherCollider.gameObject.tag == "Player" && contact.otherCollider.gameObject != origin){
 		    print("Hit player");
 		    // Apply damage
 			contact.otherCollider.gameObject.GetComponent<PlayerDelegate>().AddDamage(kineticDamage);
@@ -46,13 +73,13 @@ public class KineticProjectile : MonoBehaviour{
 		    contact.otherCollider.gameObject.transform.parent.GetComponent<Shield>().Impact(kineticDamage);
 		}
 		source.PlayOneShot(impactSound);
-		Destroy(gameObject);
+		if(returns){
+			returning = true;
+		}
 	}
 
-	// Propel the projectile towards the nearest
-	// opponent in view
-	void SeekOpponent(){
-		GameObject opponent = FindOpponent();
+	// Propel the projectile towards the desired ship
+	void SeekShip(GameObject opponent){
 		if(opponent != null){
 			Vector3 distanceVector = (opponent.transform.position-transform.position);
 			GetComponent<Rigidbody>().AddForce(
@@ -92,6 +119,10 @@ public class KineticProjectile : MonoBehaviour{
 			}
 		}
 		return targetShip;
+	}
+
+	private void Return(){
+		
 	}
 
     // Triggered when this projectile explodes
